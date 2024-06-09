4ae4f4eb66e88d914ab44585847071484ddccf53
Naytchead <Naytchead@hotmail.com>
2024-06-09 16:15:53 +0200

    [Component] Add Component Toggler with all logique and display (oneshot)

---
+++ src/components/toggler/toggler.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react';
+import './style/toggler.style.css';
+
+export const Toggler = (props) => {
+    const { handleClickArrow, titleHeader, contents, toggled, id } = props;
+    const [contentToggle, setContentToggle] = useState(toggled);
+    const contentsMap = () =>
+        contents.map((content, index) => <p key={index}>{content}</p>);
+    let classUsed = 'container';
+    useEffect(() => {
+        setContentToggle(toggled);
+        // return () => contentToggle;
+    }, [toggled]);
+    const initContent = () => {
+        if (Array.isArray(contents)) {
+            return contentsMap();
+        }
+        return <p>{contents}</p>;
+    };
+    console.log('contentToggled:', contentToggle, classUsed);
+    return (
+        <div className="toggler" key={id}>
+            <header>
+                <h3>{titleHeader}</h3>
+                <img
+                    class="arrow-back"
+                    src="/images/arrow_back.svg"
+                    onClick={(e) => handleClickArrow(e, id)}
+                />
+            </header>
+            <div className={`container ${contentToggle ? 'toggled' : ''}`}>
+                {toggled ? initContent() : ''}
+            </div>
+        </div>
+    );
+};
